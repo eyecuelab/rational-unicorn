4fa6fe04e9970a1d2d321132695556f806dab53e
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-08 16:54:59 -0700

    save in case of power outage.

--- src/components/results.js
+++ src/components/results.js
@@ -1,11 +1,12 @@
 import React from "react"
 
 const Results = ({ value }) => {
-  let result = []
+  let result = [] // let result = initial for loop and then instead of result.push we would have the <p> around value[1].text Hmmmmmmmm yeah mayb? I don't know how that will jive with the html/jsx.
   for (let i = 1; i < value.length; i++) {
     console.log(value[i].text)
         value[i].category !== "void" ? result.push(value[i].text) : result
   }
+  console.table(result)
   return result
 }
 
