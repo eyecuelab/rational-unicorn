68feee2758a79a0e29d434df86f4bc9688aecc71
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-10 13:45:20 -0700

    add css styling to modal component.

--- src/components/descriptionModal.js
+++ src/components/descriptionModal.js
@@ -5,18 +5,57 @@ const DescriptionModal = ({ onHide, value, handleClick })=> {
 
   return (
     <>
+      <style type="text/css">
+      {`
+      .btn-close {
+        background-color: rgb(42, 196, 175);
+        color: white;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100px;
+        height: 30px
+
+      }
+
+      .btn-confirm {
+        background-color: rgb(28, 129, 116);
+        color: white;
+        border-radius: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100px;
+        height: 30px
+      }
+
+      .modal-header {
+        background-color: purple;
+        color: white;
+        font-size: 10px;
+        padding: 0.5em, 1.5em;
+        text-align: center;
+      }
+
+      .modal-footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      `}
+      </style>
       <Modal show={true} onHide={onHide}>
         <Modal.Header closeButton>
           <Modal.Title>{value.text}</Modal.Title>
         </Modal.Header>
         <Modal.Body>{value.description}</Modal.Body>
         <Modal.Footer>
-          <p>Is this what you were looking for?</p>
-          <Button variant="outline-dark" onClick={onHide}>
-            Nope
+          <Button variant="close" onClick={onHide}>
+            Close
           </Button>
-          <Button variant="outline-success" onClick={()=>handleClick(value)}>
-            Yup!
+          <Button variant="confirm" onClick={()=>handleClick(value)}>
+            Confirm
           </Button>
         </Modal.Footer>
       </Modal>
