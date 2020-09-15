605fc1f2fc03abe7a00d919785cf406a19813003
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-14 22:36:20 -0700

    add if statement to usePrevNode to prevent error when hitting back button on first node.

--- src/components/usePrevNode.js
+++ src/components/usePrevNode.js
@@ -2,12 +2,16 @@ import TextNodes from "../components/content"
 
 const usePrevNode = (value) => {
 
+    console.log(value.length, "____value.length____")
+
     const lastPath = value.length -1;
     for (let i = 0; i < TextNodes.length; i++) {
-        if (value[lastPath].prevNodeId == TextNodes[i].nodeId) {
+        if (value?.[lastPath]?.prevNodeId == TextNodes[i].nodeId) {
             return ( 
                 TextNodes[i]
-            )
+            ) 
+        } else {
+            window.location = 'http://localhost:8000/';
         }
     } 
 }
