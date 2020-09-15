f2d105c94a406e78d7e1f2d680c767e2f4de9e54
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-14 18:41:56 -0700

    add logo to background.

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,15 +7,17 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <div className="splashContainer">
-      <h1>Choose your venture</h1>
-      <h2>An Interactive Guide To Your First Business Entity</h2>
-    </div>
-    <div className='buttonContainer'>
-      <button class="start-btn">
-        <Link to="/helper/">Start</Link>
-      </button>
+    <div class="container">
+      <SEO title="Home" />
+      <div className="splashContainer">
+        <h1>Choose your venture</h1>
+        <h2>An Interactive Guide To Your First Business Entity</h2>
+      </div>
+      <div className='buttonContainer'>
+        <button class="start-btn">
+          <Link to="/helper/">Start</Link>
+        </button>
+      </div>
     </div>
   </Layout>
 )
