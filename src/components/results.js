9e77ddb09b3fba782a28b52529f93da03cb0aa03
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-15 18:14:08 -0700

    customize list item bullets on results page.

--- src/components/results.js
+++ src/components/results.js
@@ -15,12 +15,40 @@ const Results = ({ value, showEmail }) => {
     // console.log(output)
 
     return (
-        <div className="result-container">
-            <h3 id="result-head">Your cheat sheet</h3>
-            {output.map(outputText => <li id="result-text">{outputText}</li>)}<br/><br/>
-    <button className="end-btn" onClick={showEmail}>Email Me</button><br/>
-            <button className="end-btn">Download PDF</button>
-        </div>
+        <>
+            <style type="text/css">
+                {`
+                    li {
+                        list-style-type: none;
+                        position: relative;
+                        padding-left: 10px;
+                        }
+                        
+                    li::before {
+                        content: "";
+                        position: absolute;
+                        top: 0;
+                        bottom: 0;
+                        left: 0;
+                        margin: auto;
+                        height: 10px;
+                        width: 10px;
+                        display: block;
+                        background-image: url(https://github.com/eyecuelab/rational-unicorn/blob/modal/src/images/arrow-white.PNG);
+                        background-repeat: no-repeat;
+                        background-size: 100%;
+                        background-color: #810F5D;
+                        transform: translate(-50%, 0%);
+                    }    
+                `}
+            </style>
+            <div className="result-container">
+                <h3 id="result-head">Your cheat sheet</h3>
+                {output.map(outputText => <li id="result-text">{outputText}</li>)}<br/><br/>
+                <button className="end-btn" onClick={showEmail}>Email Me</button><br/>
+                <button className="end-btn">Download PDF</button>
+            </div>
+        </>
     )
 } 
 
