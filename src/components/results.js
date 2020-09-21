e76614614569eecdfd222596694c43e6e81899c6
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-21 15:25:23 -0700

    update pdf download function.

--- src/components/results.js
+++ src/components/results.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Results = ({ value, showEmail }) => {
+const Results = ({ value, showEmail, download }) => {
 
     const output = value.reduce((outputTextArray, currentValue) => {
         const returnArray = [...outputTextArray];
@@ -15,11 +15,11 @@ const Results = ({ value, showEmail }) => {
     return (
         <>
             <div className="resultUnicron" />
-            <div className="result-container">
-                <h3 id="result-head">Your cheat sheet</h3>
+            <div className="results-container">
+                <h3 className="results-head">Your cheat sheet</h3>
                 {output.map(outputText => <li className="results-li">{outputText}</li>)}<br/><br/>
                 <button className="end-btn1" onClick={showEmail}><p className="end-btn1-text">Email Me</p></button><br/>
-                <button className="end-btn2"><p className="end-btn2-text">Download PDF</p></button>
+                <button className="end-btn2" onClick={download}><p className="end-btn2-text">Download PDF</p></button>
             </div>
         </>
     )
