ccfbb356cbc2fcccf6febba2811bf385f4ad56c1
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-23 11:13:45 -0700

    successfully implement new menu with custom animation.

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,6 @@
 import React, { useState } from "react"
 import Menu from "../components/menu"
-// import { Navbar, Nav, NavDropdown } from "react-bootstrap"
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-// import { faHorseHead, faPaperPlane } from "@fortawesome/free-solid-svg-icons"
-// import { Link } from "gatsby"
 
-// const navImg = {
-//   width: "70px",
-// }
-// const navLinkStyles = {
-//   marginLeft: "10%",
-//   fontFamily: "Montserrat",
-//   fontStyle: "normal",
-//   fontWeight: "bold",
-//   fontSize: "20px",
-//   lineHeight: "20px",
-//   color: "#FFFFFF",
-// }
 
 const Header = () => {
 
@@ -26,63 +10,11 @@ const Header = () => {
     <>
       <button id="logoButton"
         onClick={()=>setMenuVisibility(true)} />
-      {menuVisibility ? (
         <Menu 
         value={menuVisibility}
         toggle={()=>setMenuVisibility(false)} /> 
-        ) : null}
     </>
   )
 }
-
-
-  // function toMainSite() {
-  //   window.location = "https://rationalunicornlegalservices.com/"
-  // }
-
-  // function toContact() {
-  //   window.location = "https://rationalunicornlegalservices.com/contact"
-  // }
-
-  
-{/* 
-  <>      <Navbar className="navBar" variant="dark" expand="lg">
-        <Navbar.Brand>
-          <Link to="/">
-            {
-              <img
-                style={navImg}
-                src="https://i.ibb.co/2nVm0Y3/Rational-Unicorn-1.png"
-                alt="Unicorn Logo"
-              />
-            }
-          </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown.Item>
-              <div className="homeLink" onClick={() => toMainSite()}>
-                <div className="navNoWrap">
-                  <FontAwesomeIcon icon={faHorseHead} />
-                  <p style={navLinkStyles}>Main Site</p>
-                </div>
-              </div>
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              <div className="contactLink" onClick={toContact}>
-                <div className="navNoWrap">
-                  <FontAwesomeIcon icon={faPaperPlane} />
-                  <p style={navLinkStyles}>Contact Us</p>
-                </div>
-              </div>
-            </NavDropdown.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar> 
-    </>
-  )
-}
-*/}
 
 export default Header
--- src/components/menu.js
+++ src/components/menu.js
@@ -12,23 +12,26 @@ const Menu = ({toggle, value}) => {
       visibility = "show"
     }
 
+    console.log(visibility, " visiblity status")
+
     return (
         <>
             <div id="flyoutMenu"
                onClick={toggle} 
                className={visibility}>
+            <br/>
             <div className="navNoWrap">
-                <h2 ><Link to="/"><FontAwesomeIcon icon={faHome}/>  Home</Link></h2>
+                <h2 ><Link to="/"><FontAwesomeIcon className="icon-font" icon={faHome}/>  Home</Link></h2>
             </div> 
             <div className="navNoWrap">
-                <h2 ><Link to="/about"><FontAwesomeIcon icon={faInfoCircle}/>  About</Link></h2>
+                <h2 ><Link to="/about"><FontAwesomeIcon className="icon-font" icon={faInfoCircle}/>  About</Link></h2>
             </div>
             <hr />
             <div className="navNoWrap">
-                <h2 ><a href="https://rationalunicornlegalservices.com/"><FontAwesomeIcon icon={faHorseHead}/>  Company Site</a></h2>
+                <h2 ><a href="https://rationalunicornlegalservices.com/"><FontAwesomeIcon className="icon-font" icon={faHorseHead}/>  Company Site</a></h2>
             </div>
             <div className="navNoWrap">
-                <h2 ><a href="https://rationalunicornlegalservices.com/contact"><FontAwesomeIcon icon={faPaperPlane}/>  Contact Us</a></h2>
+                <h2 ><a href="https://rationalunicornlegalservices.com/contact"><FontAwesomeIcon className="icon-font" icon={faPaperPlane}/>  Contact Us</a></h2>
             </div>
           </div>
         </>
