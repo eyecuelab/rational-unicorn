3262e24f77840bfe71551f9a9edd037253e52957
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-24 17:48:30 -0700

    merging front end with cms.

--- src/components/results.js
+++ src/components/results.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Results = ({ value, showEmail, download }) => {
+const Results = ({ value }) => {
 
     const output = value.reduce((outputTextArray, currentValue) => {
         const returnArray = [...outputTextArray];
@@ -8,20 +8,16 @@ const Results = ({ value, showEmail, download }) => {
         if (resultText && resultText !== 'void') {
             returnArray.push(resultText);
         }
-
+        // console.log(currentValue, returnArray, outputTextArray);
         return returnArray;
     }, []);
 
+    // console.log(output)
+
     return (
-        <>
-            <div className="resultUnicron" />
-            <div className="results-container">
-                <h3 className="results-head">Your cheat sheet</h3>
-                {output.map(outputText => <li className="results-li">{outputText}</li>)}<br/><br/>
-                <button className="end-btn1" onClick={showEmail}><p className="end-btn1-text">Email Me</p></button><br/>
-                <button className="end-btn2" onClick={download}><p className="end-btn2-text">Download PDF</p></button>
-            </div>
-        </>
+        <ul>
+            {output.map(outputText => <li>{outputText}</li>)}
+        </ul>
     )
 } 
 
