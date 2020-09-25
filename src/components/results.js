772e23f2bd8269d90c37b057acd89ee6fadf4d12
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-24 17:51:59 -0700

    bug fixing repo issues.

--- src/components/results.js
+++ src/components/results.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-const Results = ({ value }) => {
+const Results = ({ value, showEmail, download }) => {
 
     const output = value.reduce((outputTextArray, currentValue) => {
         const returnArray = [...outputTextArray];
@@ -8,16 +8,21 @@ const Results = ({ value }) => {
         if (resultText && resultText !== 'void') {
             returnArray.push(resultText);
         }
-        // console.log(currentValue, returnArray, outputTextArray);
+
         return returnArray;
     }, []);
 
-    // console.log(output)
-
     return (
-        <ul>
-            {output.map(outputText => <li>{outputText}</li>)}
-        </ul>
+        <>
+             <div className="resultUnicron" /> 
+            <div className="results-container">
+                <h3 className="results-head">Your cheat sheet</h3>
+                {output.map((outputText) => <li key={outputText?.replace(/\s/,'-')} className="results-li">{outputText}</li>)}<br/><br/>
+                <button className="end-btn1" onClick={showEmail}><p className="end-btn1-text">Email Me</p></button><br/>
+                {/* <button className="end-btn2"><p className="end-btn2-text">Make Appointment</p></button> */}
+                <button className="end-btn2" onClick={download}><p className="end-btn2-text">Download PDF</p></button>
+            </div>
+        </>
     )
 } 
 
