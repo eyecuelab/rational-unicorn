8a4e9af6d83d9d27fdc84700300577034604f0f6
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-09-24 19:01:46 -0700

    sync up code from working branch.

--- src/pages/helper.js
+++ src/pages/helper.js
@@ -10,7 +10,6 @@ import usePrevNode from "../components/usePrevNode"
 import Results from "../components/results"
 import { reactLocalStorage } from "reactjs-localstorage"
 import { useStaticQuery } from 'gatsby'
-import { reactLocalStorage } from "reactjs-localstorage"
 import html2canvas from 'html2canvas';
 import { jsPDF } from "jspdf";
 // import ResultsPDF from "../components/resultsPDF";
@@ -127,15 +126,16 @@ const Helper = () => {
     setNodeState(nextNode)
   }
 
-  //   const downloadPDF = () => {
-  //     const divToDisplay = document.getElementById('capture')
-  //     html2canvas(divToDisplay).then(async(canvas) => {
-  //       const divImage = await canvas.toDataURL("image/png");
-  //       const pdf = new jsPDF();
-  //       await pdf.addImage(divImage, 'PNG', 0, 0);
-  //       await pdf.save("download.pdf");
-  //     })
-  //  }
+  const downloadPDF = () => {
+    const divToDisplay = document.getElementById("capture")
+    console.log(divToDisplay, "  div to display")
+    html2canvas(divToDisplay).then(async(canvas) => {
+      const divImage = await canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      await pdf.addImage(divImage, 'PNG', 0, 0);
+      await pdf.save("unicorn-results.pdf");
+    })
+ }
 
   const handleClose = () => setShowModal(false)
 
@@ -211,7 +211,7 @@ const Helper = () => {
                     showEmail={
                       (() => setShowResults(true), () => setShowModal(true))
                     }
-                  // download={downloadPDF()}
+                  download={downloadPDF()}
                   />
                   {/* <div id='capture'><ResultsPDF value={pathStorage}/></div> */}
                 </>
