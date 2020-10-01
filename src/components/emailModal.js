f19733f2bd7c4e0c2ac4ee167fe74e649439a3de
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-10-01 12:57:43 -0700

    correct pdf unicorn format.

--- src/components/emailModal.js
+++ src/components/emailModal.js
@@ -1,26 +1,25 @@
-import React, { useState } from "react";
-import {Modal, Button, Form, Col} from 'react-bootstrap';
+import React, { useState } from "react"
+import { Modal, Button, Form, Col } from "react-bootstrap"
 
 const Results = ({ onHide, value }) => {
+  const [email, setEmail] = useState("")
 
-    const [email, setEmail] = useState('')
+  const output = value.reduce((outputTextArray, currentValue) => {
+    const returnArray = [...outputTextArray]
+    const { resultText } = currentValue || {}
+    if (resultText && resultText !== "void") {
+      returnArray.push(resultText)
+    }
+    return returnArray
+  }, [])
 
-    const output = value.reduce((outputTextArray, currentValue) => {
-        const returnArray = [...outputTextArray];
-        const { resultText } = currentValue || {};
-        if (resultText && resultText !== 'void') {
-            returnArray.push(resultText);
-        }
-        return returnArray;
-    }, []);
-
-    const sendEmail = (event) => {
-        event.preventDefault();
-        const refText = "here"
-        const refLink = refText.link("https://rationalunicornlegalservices.com/referrals")
-        const listText = output.map((outputText) => `%0D%0A •  ${outputText} %0D%0A`) 
-        let a = document.createElement('a');
-        a.href=`mailto:${email}?subject=Your Cheat Sheet from Rational Unicorn&body=Congratulations on your first step to becoming an entrepreneur!
+  const sendEmail = event => {
+    event.preventDefault()
+    // const refText = "here"
+    // const refLink = refText.link("https://rationalunicornlegalservices.com/referrals")
+    const listText = output.map(outputText => `%0D%0A •  ${outputText} %0D%0A`)
+    let a = document.createElement("a")
+    a.href = `mailto:${email}?subject=Your Cheat Sheet from Rational Unicorn&body=Congratulations on your first step to becoming an entrepreneur!
         %0D%0A
         %0D%0AAt Rational Unicorn, we know it’s a daunting task to sort out all the legal requirements on your own. This is why we created this app to help potential business owners like you through the process.
         %0D%0A
@@ -28,17 +27,17 @@ const Results = ({ onHide, value }) => {
         %0D%0A ${listText}
         %0D%0A Here are some additional resources to help get you on your way:
         %0D%0A
-        %0D%0A Referrals to vetted professionals that offer additional services can be found ${refLink}.`
-        a.target = '_blank'; // this opens in a new tab
-        a.rel ='noopener noreferrer'
-        a.click();
-        onHide;
-    }
+        %0D%0A Referrals to vetted professionals that offer additional services can be found at https://rationalunicornlegalservices.com/referrals.`
+    a.target = "_blank" // this opens in a new tab
+    a.rel = "noopener noreferrer"
+    a.click()
+    onHide
+  }
 
-    return (
-        <>
-        <style type="text/css">
-      {`
+  return (
+    <>
+      <style type="text/css">
+        {`
         .btn-close {
           background-color: rgb(46, 155, 149);
           color: white;
@@ -94,45 +93,62 @@ const Results = ({ onHide, value }) => {
         }
       `}
       </style>
-        <Modal show={true} onHide={onHide}>
-            <Modal.Header>
-                <Modal.Title>
-                    <p id='email-head'>Choose your venture</p>
-                    <p id='email-title'>Results Cheat Sheet</p>
-                    <p id='email-subTitle'>Presented by Rational Unicorn</p>
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <ul id="email-body">
-                    {output.map(outputText => <><li key={outputText?.replace(/\s/,'-')} className="email-li">{outputText}</li><br/></>)}
-                </ul>
-            </Modal.Body>
-            <Modal.Footer>
-                <Form onSubmit={sendEmail}>
-                    <Form.Group>
-                        <Form.Row>
-                            <Col xs={12}>
-                                <Form.Control type="email" onChange= {(e)=> setEmail(e.target.value)} placeholder="Enter E-mail Here" /><br/>
-                                <Form.Text className="text-muted">
-                                    * We'll never share your email with anyone else *
-                                </Form.Text>
-                                <a className="newsLink" href="https://rationalunicorn.us7.list-manage.com/subscribe?u=26217361a595e76edc25043f3&id=09deb29ea3">Sign up for the Newsletter!</a>
-                            </Col>
-                        </Form.Row>
-                    </Form.Group>
-                    <div className="noWrap">
-                        <Button variant="close" onClick={onHide}>
-                            Close
-                        </Button>
-                        <Button variant="send" type="submit">
-                            Send
-                        </Button>
-                    </div>
-                </Form>
-            </Modal.Footer>
-        </Modal>
-        </>
-    );
-} 
+      <Modal show={true} onHide={onHide}>
+        <Modal.Header>
+          <Modal.Title>
+            <p id="email-head">Choose your venture</p>
+            <p id="email-title">Results Cheat Sheet</p>
+            <p id="email-subTitle">Presented by Rational Unicorn</p>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul id="email-body">
+            {output.map(outputText => (
+              <>
+                <li key={outputText?.replace(/\s/, "-")} className="email-li">
+                  {outputText}
+                </li>
+                <br />
+              </>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <Form onSubmit={sendEmail}>
+            <Form.Group>
+              <Form.Row>
+                <Col xs={12}>
+                  <Form.Control
+                    type="email"
+                    onChange={e => setEmail(e.target.value)}
+                    placeholder="Enter E-mail Here"
+                  />
+                  <br />
+                  <Form.Text className="text-muted">
+                    * We'll never share your email with anyone else *
+                  </Form.Text>
+                  <a
+                    className="newsLink"
+                    href="https://rationalunicorn.us7.list-manage.com/subscribe?u=26217361a595e76edc25043f3&id=09deb29ea3"
+                  >
+                    Sign up for the Newsletter!
+                  </a>
+                </Col>
+              </Form.Row>
+            </Form.Group>
+            <div className="noWrap">
+              <Button variant="close" onClick={onHide}>
+                Close
+              </Button>
+              <Button variant="send" type="submit">
+                Send
+              </Button>
+            </div>
+          </Form>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
 
-export default Results;
+export default Results
--- src/components/instructionsModal.js
+++ src/components/instructionsModal.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Modal, Button } from 'react-bootstrap';
+import { Modal, Button } from "react-bootstrap"
 import "../components/layout.css"
 
 const InstructionsModal = ({ onHide, node }) => {
@@ -7,20 +7,6 @@ const InstructionsModal = ({ onHide, node }) => {
     <>
       <style type="text/css">
         {`
-        .btn-close {
-          background-color: rgb(46, 155, 149);
-          color: white;
-          border-radius: 30px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 120px;
-          height: 30px;
-          transform: scale(1.1);
-        }
-        .btn-close:active{
-          background-color: rgb(18, 63, 61);
-        }
         .head {
           font-size:16px;
           font-weight:bold
@@ -28,6 +14,7 @@ const InstructionsModal = ({ onHide, node }) => {
         .details {
           font-size:14px;
           font-family: Montserrat;
+          color:white
         }
         .details-nodeId {
           font-size: 14px;
@@ -35,14 +22,18 @@ const InstructionsModal = ({ onHide, node }) => {
           font-family:Montserrat
         }
         p {
-          font-size:12px
+          font-size:12px;
+          color: white
         }
       `}
       </style>
+
       <Modal show={true} onHide={onHide}>
         <Modal.Header>
           <Modal.Title>
-            <p class="head">Details for: <span class="details">{node.node.nodeId}</span></p>
+            <p class="head">
+              Details for: <span class="details">{node.node.nodeId}</span>
+            </p>
           </Modal.Title>
           <Modal.Body>
             <p class="head">Question/Prompt:</p>
@@ -50,14 +41,20 @@ const InstructionsModal = ({ onHide, node }) => {
             <div>
               <p class="head">Answers:</p>
               {node.node.answers.answers.map(answer => {
-                return <div><span class="details">"{answer.text}" </span>
-                  <p>links to: <span class="details-nodeId">{answer.nextNodeId}</span></p></div>
+                return (
+                  <div>
+                    <span class="details">"{answer.text}" </span>
+                    <p>
+                      links to:{" "}
+                      <span class="details-nodeId">{answer.nextNodeId}</span>
+                    </p>
+                  </div>
+                )
               })}
-
             </div>
           </Modal.Body>
         </Modal.Header>
-      </Modal >
+      </Modal>
     </>
   )
 }
