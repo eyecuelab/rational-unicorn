644648560b2dda34fbe5c20ebcff2df4cfc00b4a
Tyler Bates <tylerbatesdevdesign@gmail.com>
2020-10-07 15:23:25 -0700

    refactor graphql content function to correlate with new contentful data structure.

--- src/pages/instructions.js
+++ src/pages/instructions.js
@@ -35,9 +35,7 @@ const Instructions = () => {
       allContentfulUnicornNode {
         edges {
           node {
-            question {
-              question
-            }
+            question
             answers {
               answers {
                 nextNodeId
